e5ed314f43afcae14bc08f1872b7950500b65ec2
Bruno Morais Guida <bruno.guida@e-unicred.com.br>
2025-05-14 23:09:13 -0300

    first

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ def api_converter():
         "horario_cotacao": horario
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+# if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=8080)
